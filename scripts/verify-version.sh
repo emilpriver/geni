746af0732a3700ef9a2f8f8bbe58e745587d52f6
Emil Privér <emil@priver.dev>
2024-01-01 18:25:39 +0100

    moved scripts files

--- scripts/verify-version.sh
+++ scripts/verify-version.sh
@@ -11,8 +11,8 @@ tag_version=$(echo "$github_tag" | grep -oP '(?<=refs/tags/v)[^"]*')
 
 # Compare the versions
 if [[ "v$cargo_version" == "$tag_version" ]]; then
-    echo "Version in Cargo.toml matches the new GitHub Actions tag."
+	echo "Version in Cargo.toml matches the new GitHub Actions tag."
 else
-    echo "Version in Cargo.toml does not match the new GitHub Actions tag."
-    exit 1
+	echo "Version in Cargo.toml does not match the new GitHub Actions tag."
+	exit 1
 fi
