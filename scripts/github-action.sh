59f15a5e2704afd77f23c946432f826420f2a205
Emil Privér <emil@priver.dev>
2024-01-05 00:57:46 +0100

    updated test

--- scripts/github-action.sh
+++ scripts/github-action.sh
@@ -9,11 +9,22 @@ LATEST_TAG=$(curl -s https://api.github.com/repos/$USER_REPO/releases/latest | g
 # Determine the OS and set the appropriate download URL
 OS="$(uname -s)"
 case "$OS" in
-    Linux*)     OS_TYPE='linux'; BINARY_SUFFIX='linux-amd64' ;;
-    Darwin*)    OS_TYPE='macos'; BINARY_SUFFIX='macos-amd64' ;;
-    CYGWIN*|MINGW32*|MSYS*|MINGW*) 
-                OS_TYPE='windows'; BINARY_SUFFIX='windows-amd64' ;;
-    *)          echo "Unsupported OS: $OS"; exit 1 ;;
+Linux*)
+	OS_TYPE='linux'
+	BINARY_SUFFIX='linux-amd64'
+	;;
+Darwin*)
+	OS_TYPE='macos'
+	BINARY_SUFFIX='macos-amd64'
+	;;
+CYGWIN* | MINGW32* | MSYS* | MINGW*)
+	OS_TYPE='windows'
+	BINARY_SUFFIX='windows-amd64'
+	;;
+*)
+	echo "Unsupported OS: $OS"
+	exit 1
+	;;
 esac
 
 DOWNLOAD_URL="https://github.com/$USER_REPO/releases/download/$LATEST_TAG/geni-$BINARY_SUFFIX"
@@ -21,16 +32,7 @@ DOWNLOAD_URL="https://github.com/$USER_REPO/releases/download/$LATEST_TAG/geni-$
 # Download and make the binary executable
 curl -L $DOWNLOAD_URL -o geni_binary
 if [ "$OS_TYPE" != "windows" ]; then
-    chmod +x geni_binary
+	chmod +x geni_binary
 fi
 
-# Set environment variables
-export DATABASE_MIGRATIONS_FOLDER="${inputs.migrations_folder}"
-export DATABASE_URL="${inputs.database_url}"
-export DATABASE_TOKEN="${inputs.database_token}"
-export DATABASE_WAIT_TIMEOUT="${inputs.wait_timeout}"
-export DATABASE_NO_DUMP_SCHEMA="false"
-export DATABASE_MIGRATIONS_TABLE="${inputs.migrations_table}"
-
 ./geni_binary up
-
