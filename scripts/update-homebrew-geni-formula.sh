f2f3e03295461f6d27aa2e06168c90b8f1d115fd
Emil Privér <emil@priver.dev>
2024-01-05 10:01:02 +0100

    fixed url change

--- scripts/update-homebrew-geni-formula.sh
+++ scripts/update-homebrew-geni-formula.sh
@@ -10,7 +10,7 @@ git clone "$REPO_URL"
 cd homebrew-geni
 
 # Update the version in geni.rb
-sed -i "s|url \".*\"|url \"https://github.com/emilpriver/geni/archive/refs/tags/v$NEW_VERSION.tar.gz\"|" $FORMULA_PATH
+sed -i "s|url \".*\"|url \"https://github.com/emilpriver/geni/archive/refs/tags/$NEW_VERSION.tar.gz\"|" $FORMULA_PATH
 
 # Get the new tarball and calculate its SHA256 checksum
 NEW_TARBALL_URL="https://github.com/emilpriver/geni/archive/refs/tags/v$NEW_VERSION.tar.gz"
