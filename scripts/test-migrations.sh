437dcb056b941a7298f55affdb6139802affb3a0
Emil Privér <emil@priver.dev>
2024-01-05 01:15:07 +0100

    removed ,

--- scripts/test-migrations.sh
+++ scripts/test-migrations.sh
@@ -8,7 +8,7 @@ for i in {1..2}; do
 	filename_up="${timestamp}${i}_test_${i}.up.sql"
 
 	echo "CREATE TABLE table_${i} (
-      name VARCHAR(255) NOT NULL,
+      name VARCHAR(255) NOT NULL
     );" >$filename_up
 
 	filename_down="${timestamp}${i}_test_${i}.down.sql"
