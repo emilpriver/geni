f0cb379d781c9a6632eaf7347e1d8d0c2e215b13
emilpriver <emil@priver.dev>
2024-02-10 12:13:27 +0100

    added test for transactions

--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -42,3 +42,40 @@ pub fn get_local_migrations(folder: &PathBuf, ending: &str) -> Result<Vec<(i64, 
 pub fn read_file_content(path: &PathBuf) -> String {
     fs::read_to_string(path).unwrap()
 }
+
+pub fn should_run_in_transaction(query: &str) -> bool {
+    !query
+        .split_once('\n')
+        .unwrap_or(("", ""))
+        .0
+        .contains("transaction: no")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_without_transaction_no_in_first_line() {
+        let query = "something else\ntransaction: no";
+        assert_eq!(should_run_in_transaction(query), true);
+    }
+
+    #[test]
+    fn test_with_empty_line() {
+        let query = "";
+        assert_eq!(should_run_in_transaction(query), true);
+    }
+
+    #[test]
+    fn test_with_transaction_yes_in_first_line() {
+        let query = "transaction: yes\nSELECT * FROM users";
+        assert_eq!(should_run_in_transaction(query), true);
+    }
+
+    #[test]
+    fn test_with_transaction_no_in_first_line() {
+        let query = "transaction: no\nSELECT * FROM users";
+        assert_eq!(should_run_in_transaction(query), false);
+    }
+}
