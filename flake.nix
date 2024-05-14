8befa153f300e70f8a48457d6df057a233f4d7ce
emilpriver <emil@priver.dev>
2024-05-14 15:27:13 +0200

    Fixed flakes

--- flake.nix
+++ flake.nix
@@ -1,25 +1,58 @@
 {
-  description = "A very basic flake";
+  description = "A very basic flake with integrated Rust project geni";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Using unstable channel for packages
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in rec {
-    packages.geni = pkgs.callPackage ./default.nix { };
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    legacyPackages = packages;
+      # Importing the package configuration from previously separate default.nix
+      geni = let
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      in pkgs.rustPlatform.buildRustPackage rec {
+        pname = "geni";
+        version = manifest.version;
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.sources.cleanSource ./.;
+        doCheck = false;
 
-    defaultPackage = packages.geni;
+        buildPhase = ''
+          cargo build --release --locked
+        '';
 
-    devShell = pkgs.mkShell {
-      CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/release/geni $out/bin/
+        '';
 
-      buildInputs = with pkgs; [ cargo rustc git ];
-    };
-  });
+        meta = with pkgs.lib; {
+          description = manifest.description;
+          longDescription = ''
+            Standalone database migration tool which works for Postgres, MariaDB, MySQL, Sqlite and LibSQL(Turso).
+          '';
+          homepage = manifest.repository;
+          changelog = "${manifest.repository}/releases/tag/v${version}";
+          license = licenses.mit;
+          maintainers = [ maintainers.emilpriver ];
+          platforms = platforms.all;
+        };
+      };
+    in rec {
+      packages.geni = geni;
+
+      legacyPackages = packages;
+
+      defaultPackage = packages.geni;
+
+      devShell = pkgs.mkShell {
+        CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
+
+        buildInputs = with pkgs; [ cargo rustc git ];
+      };
+    });
 }
+
